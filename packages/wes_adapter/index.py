321501762705a3e4bf0fd6c57c0d55887aa5c983
Cloudy Lopez <lcloudy@amazon.com>
2022-02-16 19:29:03 -0800

    stops wsgi from sending Json as a Base64 string

--- packages/wes_adapter/index.py
+++ packages/wes_adapter/index.py
@@ -3,7 +3,7 @@ from rest_api import create_app
 
 app = create_app()
 
-# stops wsgi from sending Json as a base64 string
+# stops wsgi from sending Json as a Base64 string
 serverless_wsgi.TEXT_MIME_TYPES.append("application/problem+json")
 
 def handler(event, context):
